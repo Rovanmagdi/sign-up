dc723de1f124c4e8e9f297196b23ff2699698e95
MohamedEid <64366119+Rovanmagdi@users.noreply.github.com>
2021-01-21 04:38:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-var from=document.getElementById("form");
-var btn=document.getElementById("btn");
+var form2=document.getElementById("myModal2");
+var signup=document.getElementById("signup");
+var form1=document.getElementById("exampleModal");
+var login=document.getElementById("login");
 
+login.addEventListener("click",getformdata1);
+function getformdata1(e){
+    e.preventDefault();
+    var formdata=new FormData(form1);
+    console.log(formdata.get('namefeild')  + " - " +formdata.get('passfeild'));
+}
 
-btn.addEventListener("click",getformdata);
+signup.addEventListener("click",getformdata);
 function getformdata(e){
     e.preventDefault();
-    var formdata=new FormData(form);
+    var formdata=new FormData(form2);
     console.log(formdata.get('namefeild') + " - " +formdata.get('emailfeild') + " - " +formdata.get('passfeild') + " - " +formdata.get('verifypassfeild'));
 }
+// //////////////////////////////////
+var password = document.getElementById("password")
+var confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+
+
+
 
